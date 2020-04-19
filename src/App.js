bc9dbaaccf4aeb855fe7be42e014c9bf9dbee04e
Nishant Kaushish <nishant@Nishants-iMac.local>
2020-04-19 16:31:29 +1000

    sample commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>I am so happy shit is working</h1>
-      </header>
+      <h1>I am so happy shit is working</h1>
     </div>
   );
 }
